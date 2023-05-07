b6dc485d9803d1ca5a3ca49ca17696e4f5d97e4d
keerthikkn <keerthikkn2000@gmail.com>
2023-05-08 01:00:21 +0530

    working html

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods = ['post'])
-def prdict_api():
+def predict_api():
     data = request.json['data']
     print(data)
     data = np.array(list(data.values())).reshape(1,-1)
@@ -20,5 +20,15 @@ def prdict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    data =np.array(data).reshape(1,-1)
+    print(data)
+    output=model.predict(data)[0]
+    return render_template("home.html",prediction_text="Estimated shipping cost is {}".format(output))
+
+
 if __name__=="__main__":
     app.run(debug=True)
+
